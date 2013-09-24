83ba9ff5954e0a75abf7fe96a9cbe1450d3b2230
trider <jonnygold@gmail.com>
2013-09-24 14:28:55 +0300

    updates and fixes

--- maps.js
+++ maps.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function ()
 {
 
@@ -8,77 +9,54 @@ $(document).ready(function ()
     setMapSize();
     var marker;
     var timer;
-    var count = 0;
     var map;
     var rides;
+    var interval = 60;
+    var count = 0;
 
-    var locopts = { watch: false, locate: true, setView: true, enableHighAccuracy: true };
-    
     $('#map, #ctrls, #btnStop, #btnHide, #txt, #aboutPage').hide();
     $('#maps').show();
 
-
-    $("#offroad").click(function ()
+    $(".ride").click(function ()
     {
-        $("#prependedRide").val("Offroad");
-    });
-    $("#onroad").click(function ()
-    {
-        $("#prependedRide").val("Onroad");
-    });
-    $("#challange").click(function ()
-    {
-        $("#prependedRide").val("Challange");
-    });
-    $("#touring").click(function ()
-    {
-        $("#prependedRide").val("Touring");
+        $("#prependedRide").val(this.id);
     });
 
-
-    $("#day1").click(function ()
+    $(".day").click(function ()
     {
-        $("#prependedDay").val("Day1");
+        $("#prependedDay").val(this.id);
     });
-    $("#day2").click(function ()
-    {
-        $("#prependedDay").val("Day2");
-    });
-    $("#day3").click(function ()
-    {
-        $("#prependedDay").val("Day3");
-    });
-    $("#day4").click(function ()
-    {
-        $("#prependedDay").val("Day4");
-    });
-    $("#day5").click(function ()
-    {
-        $("#prependedDay").val("Day5");
-    });
-
 
     $("#btnChoose").click(function ()
     {
         $('#maps').hide();
         $('#map, #ctrls').show();
-        var val = $('#prependedRide') .val() + '_' + $('#prependedDay') .val();
+        var val = $('#prependedRide').val() + '_' + $('#prependedDay').val();
         $.get('ride_data.csv', function (csv)
         {
             var rides = $.csv.toObjects(csv);
-            $.each(rides, function (i, json){
+            $.each(rides, function (i, json)
+            {
                 if (json.name === val)
                 {
-                    map = createMap(map, json);        
+                    map = createMap(map, json);
                 }
-            }); 
-         }); 
+            });
+        });
     });
 
     $("#btnAbout").click(function ()
     {
         $('#maps').hide();
         $('#aboutPage').show();
+        $('#aboutTxt').html('<p>Created by Jonathan Gold for the Wheels of Love 2013 Charity Ride for the children of the Alyn hospital.</p>');
+    });
+
+    $("#btnDonate").click(function ()
+    {
+        $('#maps').hide();
+        $('#aboutPage').show();
+        $('#aboutTxt').html('<p>To sponsor my ride, click <a href="https://www.alynride.org/portal/riderDetails.aspx?lang=en&id=4dd738f7-7eed-e211-b7e1-000c29c4aef4">here</a>.</p>');
     });
 
     $("#btnBack").click(function ()
@@ -111,22 +89,27 @@ $(document).ready(function ()
 
         $('#btnStart').hide();
         $('#btnStop').show();
-        map.getZoom();
+
         timer = $.timer(function ()
         {
             count++;
-
-            //$('#counter').html('<b>Elapsed time:</b> ' + count + ' seconds');
-            map.on("locationfound", function (location)
+            $('#counter').html('<b>Elapsed time:</b> ' + count + ' seconds');
+            if (count % interval == 0 || count == 1)
             {
-               if (!marker)
-                    marker = L.userMarker(location.latlng, { pulsing: true, accuracy: 500, smallIcon: true
-                }).addTo(map);
-                marker.setLatLng(location.latlng);
-                marker.setAccuracy(location.accuracy);
-                marker.addTo(map);
-            });
-            map.locate(locopts);
+                console.log(count);
+                map.on("locationfound", function (location)
+                {
+                    if (!marker)
+                        marker = L.userMarker(location.latlng, { pulsing: true, accuracy: 500, smallIcon: true
+                        }).addTo(map);
+                    marker.setLatLng(location.latlng);
+                    marker.setAccuracy(location.accuracy);
+                    marker.addTo(map);
+                });
+                var zm = map.getZoom();
+                map.locate({ watch: false, locate: true, setView: true, enableHighAccuracy: true, maxZoom: zm });
+            }
+
         });
         timer.set({ time: 1000, autostart: true });
     });
@@ -147,16 +130,14 @@ function createMap(map, json)
     var map = new L.map('map', 
     {
         center: [json.lng, json.lat],
-        zoom: 10
+        zoom: 13
     });
 
     var osmTile = "http://{s}.tile.cloudmade.com/ba8af3a046054cefaed65ea8ca002dc1/101270/256/{z}/{x}/{y}.png";
     var osmCopyright = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap contributors</a>';
-                        '<a href="http://creativecommons.org/licenses/by-sa/2.0/"></a>CC-BY-SA</a>, Imagery Â© <a href="http://cloudmade.com">CloudMade</a>';
-    
+                        '<a href="http://creativecommons.org/licenses/by-sa/2.0/"></a>CC-BY-SA</a>, Imagery Â© <a href="http://cloudmade.com">CloudMade</a>'; 
     var osmLayer = new L.TileLayer(osmTile, { maxZoom: 17, attribution: osmCopyright });  
     map.addLayer(osmLayer);
-    
     addLocateControl(map);
     if ($('#elv').is(':checked')) {
        showElevation(map, json.gpx_file);
@@ -185,23 +166,6 @@ function addPath(map,path)
 
 }
 
-
-function setMarker(map, marker)
-{
-   
-    map.on("locationfound", function (location)
-        {
-            if (!marker)
-                marker = L.userMarker(location.latlng, { pulsing: true, accuracy: 500,
-                    smallIcon: true
-                }).addTo(map);
-            marker.setLatLng(location.latlng);
-            marker.setAccuracy(location.accuracy);
-            marker.addTo(map);
-        });
-    return marker
-}
-
 function addLocateControl(map)
 {
     
@@ -222,27 +186,19 @@ function addLocateControl(map)
 function showElevation(map, path)
 {
     
-        var el = L.control.elevation({
+    var el = L.control.elevation({
         position: "topright",
-        theme: "lime-theme", //default: lime-theme
+        theme: "lime-theme", 
         width: 125,
         height: 100,
-        margins: {
-            top: 10,
-            right: 10,
-            bottom: 20,
-            left: 10
-        },
-        useHeightIndicator: true, //if false a marker is drawn at map position
-        interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-        hoverNumber: {
-            decimalsX: 3, //decimals on distance (always in km)
-            decimalsY: 0, //deciamls on height (always in m)
-            formatter: undefined //custom formatter function may be injected
-        },
-        xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-        yTicks: undefined //number of ticks on y axis, calculated by default according to height
+        margins: { top: 10, right: 10, bottom: 20, left: 10 },
+        useHeightIndicator: true, 
+        interpolation: "linear", 
+        hoverNumber: { decimalsX: 3, decimalsY: 0 },
+        xTicks: undefined,
+        yTicks: undefined
     });
+
     var g=new L.GPX(path, {async: true});
     g.on("addline", function (e)
     {
@@ -291,4 +247,19 @@ function setMapSize(){
  	  $("#map").height('400px');
  	}
      
+}
+
+function formatTime(time) {
+    time = time / 10;
+    var min = parseInt(time / 6000),
+        sec = parseInt(time / 100) - min,
+        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+
+    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
+}
+
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {str = '0' + str;}
+    return str;
 }
